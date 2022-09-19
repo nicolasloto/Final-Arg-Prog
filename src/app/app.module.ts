91caf7138035f332faaa95665cb9a147727c1709
Nicolás Loto <busslxtx@gmail.com>
2022-09-19 01:38:44 -0300

    creación de header
    logo, redes y banner animado (gif)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    ContactComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
