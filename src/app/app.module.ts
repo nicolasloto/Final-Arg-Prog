da33cdabce08fd6ce9c23064f5697d9639d68707
Nicolás Loto <busslxtx@gmail.com>
2022-09-27 05:55:33 -0300

    creación de "experiencia" + "educación" + "skills"
    se unificó la sección "contacto" con "logo"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
-import { ContactComponent } from './components/contact/contact.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { AboutComponent } from './components/about/about.component';
+import { WorkComponent } from './components/work/work.component';
+import { EduComponent } from './components/edu/edu.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BannerComponent,
-    ContactComponent,
     LogoComponent,
-    AboutComponent
+    AboutComponent,
+    WorkComponent,
+    EduComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgCircleProgressModule.forRoot({}),
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/work/work.component.spec.ts
+++ src/app/components/work/work.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ContactComponent } from './contact.component';
+import { WorkComponent } from './work.component';
 
-describe('ContactComponent', () => {
-  let component: ContactComponent;
-  let fixture: ComponentFixture<ContactComponent>;
+describe('WorkComponent', () => {
+  let component: WorkComponent;
+  let fixture: ComponentFixture<WorkComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ContactComponent ]
+      declarations: [ WorkComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ContactComponent);
+    fixture = TestBed.createComponent(WorkComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
