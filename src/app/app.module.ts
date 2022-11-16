4041b537ae3f37f21108862dc8984748222feac1
Nicolás Loto <busslxtx@gmail.com>
2022-11-16 14:59:57 -0300

    routing + implementación del login en front
    cambios mínimos de márgenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,9 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { SkillsComponent } from './components/skills/skills.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -24,12 +27,15 @@ import { ProjectsComponent } from './components/projects/projects.component';
     EduComponent,
     SkillsComponent,
     FooterComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     NgCircleProgressModule.forRoot({}),
     AppRoutingModule,
+    HttpClientModule
 
   ],
   providers: [],
